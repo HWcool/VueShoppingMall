520e27cbb9c292049838785a979d00cb48f8b373
HJ <137892569@qq.com>
2018-04-17 17:00:13 +0800

    工程初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { CellSwipe } from "vant";
+
+Vue.use(CellSwipe);
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: ""
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
