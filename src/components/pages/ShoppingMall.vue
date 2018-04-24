6ceb65a34e9faade8ebc66894b09a05fd1ba967a
HJ <137892569@qq.com>
2018-04-24 17:27:32 +0800

    banner

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -2,22 +2,43 @@
   <div class="container">
     <van-row>
       <Searchbar></Searchbar>
+      <!-- Swiper -->
+      <div class="swiper-area">
+        <van-swipe :autoplay="3000">
+          <van-swipe-item v-for="(banner, index) in bannerPicArray" :key="index">
+            <img v-lazy = "banner.imageUrl" width="100%" />
+          </van-swipe-item>
+        </van-swipe>
+      </div>
       <Footer></Footer>
     </van-row>
   </div>
 </template>
 
 <script>
-import Searchbar from './Search-bar'
-import Footer from './Footer'
-  export default {
-   components:{
-     Searchbar,
-     Footer,
-   }
+import Searchbar from "./Search-bar";
+import Footer from "./Footer";
+export default {
+  data() {
+    return {
+      bannerPicArray:[
+                {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
+                {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
+                {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'},
+            ]
+    }
+  },
+  components: {
+    Searchbar,
+    Footer
   }
+};
 </script>
 
 <style scoped>
-
+.swiper-area{
+  width: 20rem;
+  clear: both;
+  overflow: hidden;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import {Button, Row, Circle, Col, List} from 'vant';
+import {Button, Row, Circle, Col, List, Swipe, SwipeItem, Lazyload} from 'vant';
 Vue.config.productionTip = false;
-Vue.use(Button).use(Circle).use(Col).use(Row).use(List);
+Vue.use(Button).use(Circle).use(Col).use(Row).use(List).use(Swipe).use(SwipeItem).use(Lazyload);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
